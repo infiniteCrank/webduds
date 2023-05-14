8277f1fa3d9d76f4c30b4f63b520d2aca65fe188
Julian Wilkison-Duran <clockworksyler@gmail.com>
2023-05-14 19:47:09 -0400

    add rgb filter to layers create image meta data

--- components/webCanvas.js
+++ components/webCanvas.js
@@ -3,56 +3,70 @@ import useImage from "use-image";
 import Konva from 'konva';
 import { useEffect, useRef } from 'react';
 
-const images = [
-    require("../assets/images/wizduds/Wizduds-Artwork-1.png"),
-    require("../assets/images/wizduds/Wizduds-Artwork-2.png"),
-    require("../assets/images/wizduds/Wizduds-Artwork-3.png"),
-    require("../assets/images/wizduds/Wizduds-Artwork-4.png"),
-    require("../assets/images/wizduds/Wizduds-Artwork-5.png"),
-    require("../assets/images/wizduds/Wizduds-Artwork-6.png"),
-    require("../assets/images/wizduds/Wizduds-Artwork-7.png"),
-    require("../assets/images/wizduds/Wizduds-Artwork-8.png"),
-    require("../assets/images/wizduds/Wizduds-Artwork-9.png"),
-    require("../assets/images/wizduds/Wizduds-Artwork-10.png"),
-    require("../assets/images/wizduds/Wizduds-Artwork-11.png"),
-    require("../assets/images/wizduds/Wizduds-Artwork-12.png"),
-    require("../assets/images/wizduds/Wizduds-Artwork-13.png"),
-    require("../assets/images/wizduds/Wizduds-Artwork-14.png"),
-];
-
-const FilterImage = ({ imageSource }) => {
+const FilterImage = ({ imageSource,imageColor }) => {
     const [image] = useImage(imageSource);
     const imageRef = useRef();
 
-    // when image is loaded we need to cache the shape
     useEffect(() => {
         if (image) {
-            // you many need to reapply cache on some props changes like shadow, stroke, etc.
             imageRef.current.cache();
         }
     }, [image]);
 
-    return (
-        <Image
-            ref={imageRef}
-            x={0}
-            y={0}
-            height={650} 
-            width={650}
-            image={image}
-            filters={[Konva.Filters.Blur]}
-            blurRadius={10}
-        />
-    );
+    if(imageColor.length>0){
+        return (
+            <Image
+                ref={imageRef}
+                x={0}
+                y={0}
+                height={650} 
+                width={650}
+                image={image}
+                filters={[Konva.Filters.RGB]}
+                red={imageColor[0]}
+                green={imageColor[1]}
+                blue={imageColor[2]}
+            />
+        );
+    }else{
+        return (
+            <Image
+                ref={imageRef}
+                x={0}
+                y={0}
+                height={650} 
+                width={650}
+                image={image}
+            />
+        );
+    }
+    
 };
 
 export default function WebCanvas() {
+
+    const images = [
+        {image:require("../assets/images/wizduds/Wizduds-Artwork-1.png"),color:[100,0,0],name:"sky"},
+        {image:require("../assets/images/wizduds/Wizduds-Artwork-2.png"),color:[],name:"clouds"},
+        {image:require("../assets/images/wizduds/Wizduds-Artwork-3.png"),color:[],name:"ship"},
+        {image:require("../assets/images/wizduds/Wizduds-Artwork-4.png"),color:[],name:"logo"},
+        {image:require("../assets/images/wizduds/Wizduds-Artwork-5.png"),color:[],name:"skin"},
+        {image:require("../assets/images/wizduds/Wizduds-Artwork-6.png"),color:[],name:"shoes"},
+        {image:require("../assets/images/wizduds/Wizduds-Artwork-7.png"),color:[],name:"hair"},
+        {image:require("../assets/images/wizduds/Wizduds-Artwork-8.png"),color:[],name:"cloths"},
+        {image:require("../assets/images/wizduds/Wizduds-Artwork-9.png"),color:[],name:"staff"},
+        {image:require("../assets/images/wizduds/Wizduds-Artwork-10.png"),color:[],name:"fireball"},
+        {image:require("../assets/images/wizduds/Wizduds-Artwork-11.png"),color:[],name:"braclet"},
+        {image:require("../assets/images/wizduds/Wizduds-Artwork-12.png"),color:[],name:"spell"},
+        {image:require("../assets/images/wizduds/Wizduds-Artwork-13.png"),color:[],name:"outline"},
+        {image:require("../assets/images/wizduds/Wizduds-Artwork-14.png"),color:[],name:"eyes"},
+    ];
 
     const CreateLayers = ()=>{
         return (
             <Layer>
                 {images.map(i => {
-                    return <FilterImage imageSource={i} key={i}/>;
+                    return <FilterImage imageSource={i.image} imageColor={i.color} key={i.image}/>;
                 })}
             </Layer>
         );
