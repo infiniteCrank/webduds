b1d77500e1376e25dbd568fc38abf2140040fad5
Julian Wilkison-Duran <clockworksyler@gmail.com>
2023-05-15 15:30:46 -0400

    you can now change the color or each layer

--- components/webCanvas.js
+++ components/webCanvas.js
@@ -1,8 +1,11 @@
 import { Stage, Layer, Image, Rect} from 'react-konva';
+import {  StyleSheet, View, Text} from 'react-native';
 import useImage from "use-image";
 import Konva from 'konva';
 import { useEffect, useRef, useState } from 'react';
-import { BlockPicker, SketchPicker } from 'react-color';
+import { SketchPicker } from 'react-color';
+import DropDownPicker from "react-native-dropdown-picker";
+import {useForm, Controller} from 'react-hook-form';
 
 const FilterImage = ({ imageSource,imageColor }) => {
     const [image] = useImage(imageSource);
@@ -64,12 +67,11 @@ export default function WebCanvas() {
     ]);
 
     const [sketchPickerColor, setSketchPickerColor] = useState({
-        r: images[0].color[0],
-        g:images[0].color[1],
-        b: images[0].color[2],
+        r: images[selectedLayer|0].color[0],
+        g:images[selectedLayer|0].color[1],
+        b: images[selectedLayer|0].color[2],
         a: "1",
     });
-
 
     const CreateLayers = ()=>{
         return (
@@ -80,6 +82,16 @@ export default function WebCanvas() {
             </Layer>
         );
     }
+
+    const [layerSelectOpen, setLayerSelectOpen] = useState(false);
+    const [selectedLayer, setSelectedLayer] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const [layers, setLayers] = useState(()=>{
+        return images.map((i,layerNumber) => {
+            return { label: i.name, value: layerNumber };
+        })
+    });
+    const { handleSubmit, control } = useForm();
 
     return (
     <div>
@@ -95,11 +107,43 @@ export default function WebCanvas() {
             </Layer>
             {CreateLayers()}
         </Stage>
+        <View>
+            <Text style={styles.label}>{images[selectedLayer].name}</Text>
+            <Controller
+                name="company"
+                defaultValue=""
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                <View style={styles.dropdownCompany}>
+                    <DropDownPicker
+                    style={styles.dropdown}
+                    open={layerSelectOpen}
+                    value={selectedLayer}
+                    items={layers}
+                    setOpen={setLayerSelectOpen}
+                    setValue={setSelectedLayer}
+                    setItems={setLayers}
+                    loading={loading}
+                    activityIndicatorColor="#5188E3"
+                    searchable={true}
+                    searchPlaceholder="Search layers"
+                    onChangeValue={onChange}
+                    zIndex={1000}
+                    zIndexInverse={3000}
+                    />
+                </View>
+            )}/>
+        </View>
+
         <div className="sketchpicker">
-            <h6>Sketch Picker</h6>
+            <Text style={{color:'#ff0000'}}>{images[selectedLayer].name}</Text>
             <div
             style={{
-                backgroundColor: `rgba(${images[0].color[0]},${images[0].color[1]},${images[0].color[2]},1)`,
+                backgroundColor: `rgba(
+                    ${images[selectedLayer].color[0]},
+                    ${images[selectedLayer].color[1]},
+                    ${images[selectedLayer].color[2]},
+                    1)`,
                 width: 100,
                 height: 50,
                 border: "2px solid white",
@@ -109,9 +153,9 @@ export default function WebCanvas() {
             <SketchPicker
                 onChange={(color) => {
                     setSketchPickerColor(color.rgb);
-                    images[0].color[0] = color.rgb.r;
-                    images[0].color[1] = color.rgb.g;
-                    images[0].color[2] = color.rgb.b;
+                    images[selectedLayer].color[0] = color.rgb.r;
+                    images[selectedLayer].color[1] = color.rgb.g;
+                    images[selectedLayer].color[2] = color.rgb.b;
                     setImages(images)
 
                 }}
@@ -121,3 +165,60 @@ export default function WebCanvas() {
     </div>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    input: {
+        borderStyle: "solid",
+        borderColor: "#B7B7B7",
+        borderRadius: 7,
+        borderWidth: 1,
+        fontSize: 15,
+        height: 50,
+        marginHorizontal: 10,
+        paddingStart: 10,
+        marginBottom: 15,
+    },
+    label: {
+        marginBottom: 7,
+        marginStart: 10,
+    },
+    placeholderStyles: {
+        color: "grey",
+    },
+    dropdownGender: {
+        marginHorizontal: 10,
+        width: "50%",
+        marginBottom: 15,
+    },
+    dropdownCompany: {
+        marginHorizontal: 10,
+        marginBottom: 15,
+    },
+    dropdown: {
+        borderColor: "#B7B7B7",
+        height: 50,
+    },
+    getStarted: {
+        backgroundColor: "#5188E3",
+        color: "white",
+        textAlign: "center",
+        marginHorizontal: 60,
+        paddingVertical: 15,
+        borderRadius: 50,
+        marginTop: 20,
+    },
+    logIn: {
+        flex: 1,
+        justifyContent: "flex-end",
+        marginBottom: 10,
+    },
+    links: {
+        textAlign: "center",
+        textDecorationLine: "underline",
+        color: "#758580",
+    },
+});
+
