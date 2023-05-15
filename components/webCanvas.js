eb03a1e81484cf98bf59ceaf715d060560c41024
Julian Wilkison-Duran <clockworksyler@gmail.com>
2023-05-15 12:20:31 -0400

    I can now edit one layers colors

--- components/webCanvas.js
+++ components/webCanvas.js
@@ -1,7 +1,8 @@
 import { Stage, Layer, Image, Rect} from 'react-konva';
 import useImage from "use-image";
 import Konva from 'konva';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { BlockPicker, SketchPicker } from 'react-color';
 
 const FilterImage = ({ imageSource,imageColor }) => {
     const [image] = useImage(imageSource);
@@ -45,9 +46,9 @@ const FilterImage = ({ imageSource,imageColor }) => {
 
 export default function WebCanvas() {
 
-    const images = [
-        {image:require("../assets/images/wizduds/Wizduds-Artwork-1.png"),color:[100,0,0],name:"sky"},
-        {image:require("../assets/images/wizduds/Wizduds-Artwork-2.png"),color:[],name:"clouds"},
+    const [images, setImages] = useState([
+        {image:require("../assets/images/wizduds/Wizduds-Artwork-1.png"),color:[25,139,220],name:"sky"},
+        {image:require("../assets/images/wizduds/Wizduds-Artwork-2.png"),color:[255,255,255],name:"clouds"},
         {image:require("../assets/images/wizduds/Wizduds-Artwork-3.png"),color:[],name:"ship"},
         {image:require("../assets/images/wizduds/Wizduds-Artwork-4.png"),color:[],name:"logo"},
         {image:require("../assets/images/wizduds/Wizduds-Artwork-5.png"),color:[],name:"skin"},
@@ -60,7 +61,15 @@ export default function WebCanvas() {
         {image:require("../assets/images/wizduds/Wizduds-Artwork-12.png"),color:[],name:"spell"},
         {image:require("../assets/images/wizduds/Wizduds-Artwork-13.png"),color:[],name:"outline"},
         {image:require("../assets/images/wizduds/Wizduds-Artwork-14.png"),color:[],name:"eyes"},
-    ];
+    ]);
+
+    const [sketchPickerColor, setSketchPickerColor] = useState({
+        r: images[0].color[0],
+        g:images[0].color[1],
+        b: images[0].color[2],
+        a: "1",
+    });
+
 
     const CreateLayers = ()=>{
         return (
@@ -73,7 +82,7 @@ export default function WebCanvas() {
     }
 
     return (
-        
+    <div>
         <Stage width={window.innerWidth} height={window.innerHeight}>
             <Layer>
                 <Rect
@@ -85,7 +94,30 @@ export default function WebCanvas() {
                     />
             </Layer>
             {CreateLayers()}
-
         </Stage>
+        <div className="sketchpicker">
+            <h6>Sketch Picker</h6>
+            <div
+            style={{
+                backgroundColor: `rgba(${images[0].color[0]},${images[0].color[1]},${images[0].color[2]},1)`,
+                width: 100,
+                height: 50,
+                border: "2px solid white",
+            }}
+            ></div>
+        
+            <SketchPicker
+                onChange={(color) => {
+                    setSketchPickerColor(color.rgb);
+                    images[0].color[0] = color.rgb.r;
+                    images[0].color[1] = color.rgb.g;
+                    images[0].color[2] = color.rgb.b;
+                    setImages(images)
+
+                }}
+                color={(sketchPickerColor)}
+            />
+        </div>
+    </div>
     );
 }
